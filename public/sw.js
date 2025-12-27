057d1bf9b3c38e836c07907663331c757fefd96a
doanngocminhquy-cell <doanngocminhquy@gmail.com>
2025-12-27 12:30:52 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,6 @@
-
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10380802
+    "zoneId": 10380820
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
